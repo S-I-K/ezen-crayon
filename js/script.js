3a762dcc2f5b87af212b2922605446447297198e
DESKTOP-BFUQA8P\KIMDAESIK <daesik99@naver.com>
2023-01-11 18:44:22 +0900

    css

--- js/script.js
+++ js/script.js
@@ -2,10 +2,10 @@ $(()=>{
     var card = new Swiper("#card-wrap", {
         effect: "cards",
         grabCursor: true,
-        // slidesPerView: 'auto',
-        // autoplay: {
-        //     delay: 2000,
-        //     disableOnInteraction: false,
-        //   },
+        slidesPerView: 'auto',
+        autoplay: {
+            delay: 3000,
+            disableOnInteraction: false,
+          },
       });
 });
