6f7ecb31e119dfac5c87457a57c6b4489791527d
DESKTOP-BFUQA8P\KIMDAESIK <daesik99@naver.com>
2023-01-11 18:37:47 +0900

    add/ header, intro

---
+++ js/script.js
@@ -0,0 +1,11 @@
+$(()=>{
+    var card = new Swiper("#card-wrap", {
+        effect: "cards",
+        grabCursor: true,
+        // slidesPerView: 'auto',
+        // autoplay: {
+        //     delay: 2000,
+        //     disableOnInteraction: false,
+        //   },
+      });
+});
